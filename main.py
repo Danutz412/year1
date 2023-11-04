57177128552ec90fe072f042fd7c6d1764c14750
danut <1gheord93@solent.ac.uk>
2023-11-04 10:54:02 +0000

    while

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ removed_apples = 0
 # loop until all apples are removed
 while removed_apples < number_of_apples_to_remove:
     # print message
-    print("Removed apple.")
+    print(" Removed apple.")
 
     # increment the apples count
     removed_apples += 1
