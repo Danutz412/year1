998b2e00a8479b8fe14eaede833815696679323b
danut <1gheord93@solent.ac.uk>
2023-11-04 10:52:23 +0000

    while

---
+++ main.py
@@ -0,0 +1,13 @@
+# ask the user how many apples to remove
+number_of_apples_to_remove = int(input("How many apples should I remove?\n"))
+
+# variable to track the removed apples
+removed_apples = 0
+
+# loop until all apples are removed
+while removed_apples < number_of_apples_to_remove:
+    # print message
+    print("Removed apple.")
+
+    # increment the apples count
+    removed_apples += 1
